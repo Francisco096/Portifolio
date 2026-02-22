d7f6b2d1db1d656cbec2ef5ae09bd4ee00b4e887
“Francisco096” <gabriellfrancisco18@gmail.com>
2026-02-22 19:16:22 -0300

    organizando o código e corrigindo bugs

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 /* MODO ESCURO (Dark Mode) */
 function toggleTheme() {
     document.body.classList.toggle("dark");
-    
+
     // Opcional: Salvar a preferência do usuário no navegador
     const isDark = document.body.classList.contains("dark");
     localStorage.setItem("theme", isDark ? "dark" : "light");
@@ -17,11 +17,11 @@ if (localStorage.getItem("theme") === "dark") {
 // Espera o site carregar para animar as barras
 document.addEventListener("DOMContentLoaded", () => {
     const bars = document.querySelectorAll('.bar');
-    
+
     bars.forEach(bar => {
         // Pega a largura definida no HTML (ex: 55%)
         const width = bar.getAttribute('data-width');
-        
+
         // Aplica a largura com um pequeno atraso para dar efeito visual
         setTimeout(() => {
             bar.style.width = width;
@@ -36,7 +36,7 @@ document.addEventListener("DOMContentLoaded", () => {
 const contactForm = document.querySelector('.contact-form');
 
 if (contactForm) {
-    contactForm.addEventListener('submit', function(event) {
+    contactForm.addEventListener('submit', function (event) {
         // Impede que a página recarregue
         event.preventDefault();
 
@@ -70,11 +70,11 @@ const statusText = document.getElementById("status");
 
 if (btnLocalizar) {
     btnLocalizar.addEventListener("click", () => {
-        
+
         // Verifica se o navegador suporta geolocalização
         if (navigator.geolocation) {
             statusText.textContent = "Obtendo sua localização...";
-            
+
             navigator.geolocation.getCurrentPosition(
                 (position) => {
                     const userLocation = {
